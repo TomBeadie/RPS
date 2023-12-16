8327d47ff4feb4ce730446a9fa47cee41fcb906f
Tom <tom@atomic82.com>
2023-12-15 23:29:16 -0500

    Define cached element references, variables, constants

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 
-
+/*-------- Constants --------*/
 const choices = ['rock', 'paper', 'scissors'];
 
+/*-------- Variables (State) --------*/
+
+let playerChoice, computerChoice, msg
+
+/*-------- Cached Element References --------*/
+
+const resultDisplayEl = document.getElementById('result-display')
+
+document.getElementById('rock').addEventListener('click', play)
+document.getElementById('paper').addEventListener('click', play)
+document.getElementById('scissors').addEventListener('click', play)
+
+function play(){
+
+console.log('clicked')
+
+}
